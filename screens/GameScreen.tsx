6d5dc17a4661fb96083cb30e85f48301f3ed22f9
Musti <safi.najjar1@gmail.com>
2025-01-15 10:02:17 +0100

    show some ui in game screen

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,26 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { PrimaryTitle } from "../components/PrimaryTitle";
 
 const GameScreen = () => {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <PrimaryTitle>Opponent's Guess</PrimaryTitle>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + */}
+        {/* - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
