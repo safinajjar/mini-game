07b89847f051bec5a9ed5cd12f9ae00e8477b8aa
Musti <safi.najjar1@gmail.com>
2025-01-15 11:34:30 +0100

    add game logic

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { PrimaryTitle } from "../components/ui/PrimaryTitle";
 import { useState } from "react";
 import { NumberContainer } from "../components/game/NumberContainer";
+import { PrimaryButton } from "../components/ui/PrimaryButton";
 
 const generateRandomNumber = (
   min: number,
@@ -21,9 +22,41 @@ type GameScreenProps = {
   userNumber: number;
 };
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 const GameScreen = ({ userNumber }: GameScreenProps) => {
-  const initalGuess = generateRandomNumber(1, 100, userNumber);
+  const initalGuess = generateRandomNumber(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState<number>(initalGuess);
+
+  const nextGuessHandler = (direction: "lower" | "higher") => {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "higher" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRndNumber = generateRandomNumber(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  };
 
   return (
     <View style={styles.screen}>
@@ -31,8 +64,14 @@ const GameScreen = ({ userNumber }: GameScreenProps) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
-        {/* + */}
-        {/* - */}
+        <View style={styles.buttonsContainer}>
+          <PrimaryButton onPress={() => nextGuessHandler("higher")}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler("lower")}>
+            -
+          </PrimaryButton>
+        </View>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
@@ -45,5 +84,9 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
